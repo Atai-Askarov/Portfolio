2d083dbdd7ca107bd9ca6f88cbcc25d23cc8183c
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2025-05-07 11:51:53 -0400

    NavBar and HeroSection are created

---
+++ portfolio/src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+   <div class="bg-[#547792] min-h-screen" >
+    <NavBar/>
+    <HeroSection/>
+    
+    <!-- <HeroSection></HeroSection> -->
+  </div>
+  </template>
+<script setup>
+import {defineAsyncComponent} from "vue"
+import HeroSection from "@/components/HeroSection.vue"
+import NavBar from "./components/NavBar.vue"
+// const NavBar = defineAsyncComponent(()=>import("@/components/NavBar.vue"))
+</script>
+
+
+
